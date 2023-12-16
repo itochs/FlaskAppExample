73699ff7afd4ec4c646fc1459b0783ca48549822
itochs <halglobe0108@gmail.com>
2023-12-16 10:10:13 +0900

    memoize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [memos, setMemos] = useState([]);
+  const postMemoRef = useRef();
+  const style_spacer = {
+    borderBottomStyle: "solid",
+    borderBottomWidth: "1",
+    borderColor: "none",
+    borderBottomColor: "black",
+  };
+  const style_block = { display: "block" };
+
+  function onMemoSubmit(e) {
+    e.preventDefault();
+    if (!postMemoRef.current) {
+      return;
+    }
+
+    const memo_content = postMemoRef.current.value.trim();
+    postMemoRef.current.value = "";
+    if (memo_content === "") {
+      return;
+    }
+
+    const parsed_memo_content = memo_content
+      .split("\n")
+      .filter((line) => line !== "");
+    setMemos([...memos, parsed_memo_content]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>YOU MEMO HERE, REMEMBER</h1>
+      </header>
+      <form
+        onSubmit={(e) => onMemoSubmit(e)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.ctrlKey) {
+            onMemoSubmit(e);
+          }
+        }}
+      >
+        <button style={style_block}>メモする</button>
+        <label style={style_block} htmlFor="memo_content">
+          メモの内容
+        </label>
+        <textarea
+          style={{ ...style_block, width: "20em", height: "10em" }}
+          id="memo_content"
+          name="memo_content"
+          ref={postMemoRef}
+        />
+      </form>
+      <article>
+        <h2>メモ一覧</h2>
+        {memos.map((memo, index) => {
+          return (
+            <>
+              <article key={index}>
+                <p>
+                  {memo.map((line) => (
+                    <>
+                      {line}
+                      <br />
+                    </>
+                  ))}
+                </p>
+              </article>
+              <div style={style_spacer} />
+            </>
+          );
+        })}
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
