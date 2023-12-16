e506fcc206da0d7bcaa30dbde4e8715f01e71fab
itochs <halglobe0108@gmail.com>
2023-12-16 14:18:38 +0900

    Add endpoints to retrieve all memos and a specific memo

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+from memos.get_memo import get_all_memos as get_all_memos_from_db
+from memos.get_memo import get_memo as get_memo_from_db
 from memos.save_memo import save_memo
 
 app = Flask(__name__)
@@ -13,3 +15,18 @@ def index():
         return {"id": id}, 200
     else:
         return {"message": "memo have no content"}, 400
+
+
+@app.route("/memos", methods=["GET"])
+def get_all_memos():
+    all_memo = get_all_memos_from_db()
+    return jsonify(all_memo)
+
+
+@app.route("/memos/<id>", methods=["GET"])
+def get_memo(id: str):
+    memo = get_memo_from_db(id)
+    if memo is None:
+        return {"message": "memo not found"}, 404
+
+    return jsonify(memo)
