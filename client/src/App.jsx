ac1c5768b9c3a3f41b3a02ae89892e6c64767f2d
itochs <halglobe0108@gmail.com>
2023-12-16 13:18:07 +0900

    fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,6 +27,14 @@ function App() {
       .split("\n")
       .filter((line) => line !== "");
     setMemos([...memos, parsed_memo_content]);
+
+    fetch("http://127.0.0.1:5000/memos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ memo: parsed_memo_content }),
+    });
   }
 
   return (
