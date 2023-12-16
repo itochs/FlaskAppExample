1432a2a70953a82b980f826ce9ade1b83f9bf20d
itochs <halglobe0108@gmail.com>
2023-12-16 12:45:43 +0900

    Add save_memo function to save memos to a file

---
+++ server/src/memos/save_memo.py
@@ -0,0 +1,20 @@
+import json
+
+from util import constant
+
+
+def save_memo(memo: list[str]) -> bool:
+    if not memo or len(memo) == 0:
+        return False
+
+    memos = None
+    with open(constant.MEMO_FILE, "r") as f:
+        try:
+            memos = json.load(f)
+        except json.decoder.JSONDecodeError:
+            memos = []
+        memos.append({"memo": memo})
+    with open(constant.MEMO_FILE, "w") as f:
+        json.dump(memos, f, indent=2, ensure_ascii=False)
+
+    return True
